00e8665b5d8ddf92c63891c829fd480634ba90b6
Grace Jia <gracejia25@gmail.com>
2021-11-18 14:32:43 -0500

    setup test framework

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+total=1
+passed=0
+
+make all
+
+for ((i=1;i<=total;i++));
+do
+    input="tests/in_$i.txt"
+    expected="tests/exp_$i.txt"
+    output="tests/out_$i.txt"
+
+    ./analyzer $input $output
+
+    if cmp -s "$expected" "$output"; then
+        let passed+=1
+        echo "TEST $i: PASSED"
+    else
+        echo "TEST $i: FAILED"
+    fi
+done
+
+echo "TESTS PASSED: $passed/$total"
